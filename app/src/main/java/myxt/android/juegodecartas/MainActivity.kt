dacee738c34955fbc6f61b588a96ba8cca92678f
Moisés Rodríguez Barrero <moyankano@hotmail.com>
2020-06-10 15:37:47 +0200

    Juego de Cartas del 20IT - Versión cronómetro y contador de partidas ganadas. Al ganar 3 partidas se para el cronómetro.

--- app/src/main/java/myxt/android/juegodecartas/MainActivity.kt
+++ app/src/main/java/myxt/android/juegodecartas/MainActivity.kt
@@ -17,29 +17,39 @@ class MainActivity : AppCompatActivity() {
     var pressed4 = false
     var pressed5 = false
     var total = 0
+    var partidasGanadas = 0
 
+    var nextValues = MutableList(5) { Random.nextInt(1, 13) }
+
+    var nombreCarta = "carta" + nextValues.get(0)
+    var nombreCarta2 = "carta" + nextValues.get(1)
+    var nombreCarta3 = "carta" + nextValues.get(2)
+    var nombreCarta4 = "carta" + nextValues.get(3)
+    var nombreCarta5 = "carta" + nextValues.get(4)
+
+    var drawableResourceId = 0
+    var drawableResourceId2 = 0
+    var drawableResourceId3 = 0
+    var drawableResourceId4 = 0
+    var drawableResourceId5 = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+    }
 
-        val nextValues = MutableList(5) { Random.nextInt(1, 13) }
-
-        val nombreCarta = "carta" + nextValues.get(0)
-        val nombreCarta2 = "carta" + nextValues.get(1)
-        val nombreCarta3 = "carta" + nextValues.get(2)
-        val nombreCarta4 = "carta" + nextValues.get(3)
-        val nombreCarta5 = "carta" + nextValues.get(4)
-
-        val drawableResourceId =
+    override fun onStart() {
+        super.onStart()
+        chronometer1.start()
+        drawableResourceId =
             this.resources.getIdentifier(nombreCarta, "drawable", this.packageName)
-        val drawableResourceId2 =
+        drawableResourceId2 =
             this.resources.getIdentifier(nombreCarta2, "drawable", this.packageName)
-        val drawableResourceId3 =
+        drawableResourceId3 =
             this.resources.getIdentifier(nombreCarta3, "drawable", this.packageName)
-        val drawableResourceId4 =
+        drawableResourceId4 =
             this.resources.getIdentifier(nombreCarta4, "drawable", this.packageName)
-        val drawableResourceId5 =
+        drawableResourceId5 =
             this.resources.getIdentifier(nombreCarta5, "drawable", this.packageName)
 
         imageView.setOnClickListener {
@@ -141,18 +151,56 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun restart() {
-        val intent = Intent(this, MainActivity::class.java)
-        this.startActivity(intent)
+        //val intent = Intent(this, MainActivity::class.java)
+        //this.startActivity(intent)
+        count = 1
+        pressed = false
+        pressed2 = false
+        pressed3 = false
+        pressed4 = false
+        pressed5 = false
+        total = 0
+
+        nextValues = MutableList(5) { Random.nextInt(1, 13) }
+
+        nombreCarta = "carta" + nextValues.get(0)
+        nombreCarta2 = "carta" + nextValues.get(1)
+        nombreCarta3 = "carta" + nextValues.get(2)
+        nombreCarta4 = "carta" + nextValues.get(3)
+        nombreCarta5 = "carta" + nextValues.get(4)
+
+        drawableResourceId =
+            this.resources.getIdentifier(nombreCarta, "drawable", this.packageName)
+        drawableResourceId2 =
+            this.resources.getIdentifier(nombreCarta2, "drawable", this.packageName)
+        drawableResourceId3 =
+            this.resources.getIdentifier(nombreCarta3, "drawable", this.packageName)
+        drawableResourceId4 =
+            this.resources.getIdentifier(nombreCarta4, "drawable", this.packageName)
+        drawableResourceId5 =
+            this.resources.getIdentifier(nombreCarta5, "drawable", this.packageName)
+
+        imageView.setImageResource(R.drawable.cruz)
+        imageView2.setImageResource(R.drawable.cruz)
+        imageView3.setImageResource(R.drawable.cruz)
+        imageView4.setImageResource(R.drawable.cruz)
+        imageView5.setImageResource(R.drawable.cruz)
+
+        editText5.setText("0")
+        if(partidasGanadas==3)
+            chronometer1.stop()
     }
 
     private fun checkAndShowFinalResult() {
-        if (total < 20)
+        if (total < 20) {
             imageView6.setImageResource(R.drawable.ganador)
-        else if (total==20)
+        }
+        else if (total==20) {
             imageView6.setImageResource(R.drawable.loading)
+            partidasGanadas++
+            editText.setText(partidasGanadas.toString())
+        }
         else
             imageView6.setImageResource(R.drawable.perdedor)
-
-        imageView6.visibility = View.VISIBLE
     }
 }
